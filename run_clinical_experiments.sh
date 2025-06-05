1c89b16977a0dd5c9ce72b72ee5167db04b1d681
AKA Math <amithjkamath@users.noreply.github.com>
2025-06-05 17:18:09 +0200

    reorganization of code;

--- run_clinical_experiments.sh
+++ run_clinical_experiments.sh
@@ -6,8 +6,8 @@ for seed in 1 2 3
 do 
    for size in 32 48 64 80 96
    do
-      python ./spleen_experiments/spleen_3d_wandb_unet.py $size $SPLEEN_DATA_DIR
-      python ./spleen_experiments/spleen_3d_wandb_unetr.py $size $SPLEEN_DATA_DIR
-      python ./spleen_experiments/spleen_3d_wandb_attention_unet.py $size $SPLEEN_DATA_DIR
+      python ./src/spleen_experiments/spleen_3d_wandb_unet.py $size $SPLEEN_DATA_DIR
+      python ./src/spleen_experiments/spleen_3d_wandb_unetr.py $size $SPLEEN_DATA_DIR
+      python ./src/spleen_experiments/spleen_3d_wandb_attention_unet.py $size $SPLEEN_DATA_DIR
    done
 done
--- run_synthetic_experiments.sh
+++ run_synthetic_experiments.sh
@@ -4,8 +4,8 @@ for run in 1 2 3
 do 
    for size in 32 48 64 80 96
    do
-      python ./synthetic_experiments/synthetic_3d_wandb_unet.py $size
-      python ./synthetic_experiments/synthetic_3d_wandb_unetr.py $size
-      python ./synthetic_experiments/synthetic_3d_wandb_attention_unet.py $size
+      python ./src/synthetic_experiments/synthetic_3d_wandb_unet.py $size
+      python ./src/synthetic_experiments/synthetic_3d_wandb_unetr.py $size
+      python ./src/synthetic_experiments/synthetic_3d_wandb_attention_unet.py $size
    done
 done 
